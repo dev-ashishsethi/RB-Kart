bdbb4206a5d5b1f7994581a92484d87aac7d9a85
dev-ashishsethi <me.ashishsethi@gmail.com>
2022-03-18 10:09:24 +0530

    feat: reactRouter

--- src/App.js
+++ src/App.js
@@ -1,41 +1,19 @@
 import "./App.css";
-import logo from "./logo.png";
-
+import {Home} from "./pages/Home/home"
+import {Route,Link,Routes} from "react-router-dom"
+import { products } from "./backend/db/products";
+import { ProductListing } from "./pages/ProductListing/ProductListing";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="mockBee logo" width="180" height="180" />
-        <h1 className="brand-title">
-          Welcome to <span>mockBee!</span>
-        </h1>
-        <p className="brand-description">
-          Get started by editing <code>src/App.js</code>
-        </p>
-        <div className="links">
-          <a
-            href="https://mockbee.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Explore mockBee
-          </a>
-          <a
-            href="https://mockbee.netlify.app/docs/api/introduction"
-            target="_blank"
-            rel="noreferrer"
-          >
-            API Documentation
-          </a>
-          <a
-            href="https://github.com/neogcamp/mockBee"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Contribute
-          </a>
-        </div>
-      </header>
+
+      <Link to="./Home">Home</Link>
+      <Link to="./Product">Product</Link>
+
+      <Routes>
+        <Route path="/Home" element={<Home/>}/>
+        <Route path="/Product" element={<ProductListing/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/ProductListing/ProductListing.jsx
@@ -0,0 +1,5 @@
+import {Link,Route} from "react-router-dom";
+export function ProductListing(){
+    
+    return <h1>This is Product Listing</h1>
+}
